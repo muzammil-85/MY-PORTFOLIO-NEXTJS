b945d447293d59a3323d47978844c06bd78237bd
muzu-85 <muzukondeth85@gmail.com>
2024-05-14 11:21:34 +0530

    add new project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+
 
 export default function Hero() {
   return (
@@ -17,17 +19,22 @@ export default function Hero() {
         <div className="bg-[#60A5FA] h-[120px] top-[200px] left-[5px] absolute w-[120px] rounded-full blur-[90px] filter "></div>
 
         <div className="SpaceGroteskRegular text-[16px] md:text-[20px] lg:text-[24px] py-5">
-          <p>
-            Let me help you grow your business and make your product look pretty
-            while at it{" "}
-          </p>
+        <div className="flex items-center">
+  <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+    <FaGithub className="text-3xl mr-4" />
+  </a>
+  <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+    <FaLinkedin className="text-3xl" />
+  </a>
+</div>
+
         </div>
         <>
           <div className="relative block group  my-7 ml-4 cursor-pointer">
             <span className="absolute inset-0 border-2  border-[#188AEC] rounded-lg"></span>
             <div className="transition bg-[#188AEC] text-white rounded-lg group-hover:-translate-x-0  group-hover:-translate-y-0 -translate-x-3 translate-y-2">
               <div className="py-3 px-10 ">
-                <a href = "mailto: connect.muzu@gmail.com" className="mt-1 text-xl">Say Hello</a>
+                <a href = "/cv.pdf" className="mt-1 text-xl">View Resume</a>
               </div>
             </div>
           </div>
